174e16ff4f32085678dd7a9358f3d0add58d6549
Kyle Hart <hartky@ohsu.edu>
2021-05-28 09:58:25 -0700

    Worked examples in _R Packages_ through section 17.2

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,15 @@
+#' Bind two factors
+#'
+#'Create a new factor from two smaller factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
